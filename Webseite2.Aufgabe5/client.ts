0cf262ff218a99e505e31d062a64121b193f06c4
Francisco <francisco.paul.reinhold@hs-furtwangen.de>
2021-12-15 17:52:17 +0100

    Create client.ts
    Verbessert

---
+++ Webseite2.Aufgabe5/client.ts
@@ -0,0 +1,31 @@
+"use strict";
+const datum = document.getElementById("datum");
+const senden = document.getElementById("enter");
+const antwort = document.getElementById("antwort");
+const url = "http://127.0.0.1:3000";
+const path = "/convertDate";
+
+send.addEventListener("click", (evt) => 
+{
+    evt.preventDefault();
+    DatetoServer();
+});
+
+async function requestDateWithGet(url) 
+{
+    let response = await fetch(url);
+    let date = await response.text();
+    return date;
+}
+async function DatetoServer() 
+{
+    let inputValue = JSON.stringify(datum.value);
+    let serverAntwort = await requestDateWithGet(url + path + `?date=${inputValue}`);
+    DatetoServer2(serverAntwort);
+}
+function DatetoServer2(serverResponse) {
+    let newDate = document.createElement("p");
+    newDate.className = "serverResponse";
+    newDate.textContent = serverResponse;
+    answerArea.appendChild(newDate);
+}
